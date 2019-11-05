7639c37c0b94007bcd989a81b8dfe187d4643410
Ukarus <juan.llanosc@utem.cl>
2019-11-04 22:08:30 -0300

    player node and enemy node, tilesets to play around

---
+++ scenes/player/Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+export (int) var run_speed = 100
+export (int) var jump_speed = -550
+export (int) var gravity = 1200
+
+var velocity = Vector2()
+var jumping = false
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func get_input():
+	velocity.x = 0
+	var jump = Input.is_action_pressed("ui_up")
+	
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += run_speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= run_speed
+		
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
