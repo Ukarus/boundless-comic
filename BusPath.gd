34705481993a48aeb12936e72c0cbce68384e296
david094 <daaf1994@gmail.com>
2019-11-24 12:26:21 -0300

    Bus

---
+++ BusPath.gd
@@ -0,0 +1,16 @@
+extends Path2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var ruta = get_node("PathFollow2D")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	ruta.set_offset(ruta.get_offset() + 600 * delta)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ scenes/tiles/FondoExterior.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+var offset0 = 0
+
+#func _ready():
+	#set_process(true)
+
+#func _process(delta):
+	#offset0 -= 60 * delta
+	#set_scroll_offset(Vector2(offset0,0))
+
